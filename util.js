d1c3a3250d87f1865bc1c375d71b21461df23ab4
Levi <you@example.com>
2024-02-21 11:58:10 +0100

    first commit

---
+++ util.js
@@ -0,0 +1,11 @@
+const emojiRange = [129380,129452];
+
+function emojisArr(nr) {
+    let emojis = [];
+    for(let i = emojiRange[0];i<=emojiRange[1];i++) {
+        emojis.push(String.fromCodePoint(i));
+    }
+
+    emojis.sort(()=>Math.random()-0.5);
+    return emojis.splice(0,nr);
+}
